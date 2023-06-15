2a38dfc617eb35b010bd14bce16a0f178234fb7e
Kaniskaa Behera <101510957+kaniskaab@users.noreply.github.com>
2023-06-15 11:37:04 +0530

    updated

--- view/auth_system/src/App.js
+++ view/auth_system/src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./templates/SignIn";
 import SignUp from "./templates/SignUp";
 import Forgot from "./templates/Forgot";
-import Dashboard2 from "./templates/DashboardD/Dashboard2";
-import Patient1 from "./templates/DashboardP/Patient1";
-import MemberDetailsPage from "./templates/DashboardP/memberDetails/MemberDetails";
-import DoctorSearch from "./templates/DashboardP/DoctorSearch";
 import DoctorRegistrationForm from "./templates/doctor/DoctorRegistration";
-import GetConsultation from "./templates/DashboardP/GetConsultation";
 import Patient from "./templates/newData/Patient";
 import Sidebar from "./templates/newData/Sidebar";
 import UpdateDetails from "./templates/newData/UpdateDetails";
@@ -25,13 +20,7 @@ function App() {
         <Routes>
           <Route path="/" Component={SignIn} />
           <Route path="/signup" Component={SignUp} />
-          <Route path="/dashboard" exact Component={Patient1} />
-          <Route path="/dashboard2" Component={Dashboard2} />
-          <Route path="/forgotP" Component={Forgot} />
-          <Route path="/memberDetails" Component={MemberDetailsPage} />
-          <Route path="/doctorSearch" Component={DoctorSearch} />
           <Route path="/doctorRegistration" Component={DoctorRegistrationForm}  />
-          <Route path="/getC" Component={GetConsultation} />
           <Route path="/patient" Component={Patient} />
           <Route path="/sidebar" Component={Sidebar} />
           <Route path="/updateDetails" Component={UpdateDetails} />
--- view/auth_system/src/templates/familyMembers/DeleteMember.js
+++ view/auth_system/src/templates/familyMembers/DeleteMember.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import { useNavigate } from 'react-router-dom';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export const Delete = () =>
  {
     const refreshToken= localStorage.getItem('token');
@@ -11,20 +12,33 @@ export const Delete = () =>
     const navigate = useNavigate();
     const HandleDelete=async (e)=>
     {
+       try{
         const response = await fetch(
-            `${process.env.REACT_APP_BASE_URL}/api/members/${userId}/family-member/${familyMember_Id}`,
-            {
-              method: "DELETE",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${refreshToken}`,
-              }
+          `${process.env.REACT_APP_BASE_URL}/api/members/${userId}/family-member/${familyMember_Id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${refreshToken}`,
             }
-          ); 
-          const data = await response.json();
-          console.log(data);
-          navigate('/familyMembers')
+          }
+        ); 
+        const data = await response.json();
+        if(response.ok)
+        {
+          toast.success(data.message)
+          console.log("okay")
+        }
+        else
+        {
+          toast.warn("Member not deleted!!")
+        }
+        navigate('/familyMembers')
 
+
+       } catch(err){
+        console.log("Error : ",err)
+       }
     }
     return(
     <Popup trigger={<button>Delete User?</button>} position="right center">
@@ -32,6 +46,7 @@ export const Delete = () =>
         <div className='row-span-1'><button onClick={HandleDelete} className='bg-red-500 px-5 rounded-xl text-xl'>Yes</button></div>
         <div className='row-span-1'><button className='bg-green-500 px-5 rounded-xl text-xl'>No</button></div>
     </div>
+    <ToastContainer />
   </Popup>
 )
   }
